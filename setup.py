2615094c332e9420e5f7e936e03c36611e56a64e
Patrick Russell <prussell94@gmail.com>
2024-09-08 19:12:49 -0400

    changed url in setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='Euro_2024_tournament-data',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'pandas',
     ],
     author='Patrick Russell',
     description='Euro 2024 Tournament Data',
-    url='https://github.com/prussell94/Euro-2024-tournament-data',
+    url='https://github.com/prussell94/Euro-2024-tournament-data.git',
 )
