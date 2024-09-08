1811879ef7c2794230615c4485087a6495de0ea4
Patrick Russell <prussell94@gmail.com>
2024-09-08 19:22:04 -0400

    upped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Euro_2024_tournament-data',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'pandas',
